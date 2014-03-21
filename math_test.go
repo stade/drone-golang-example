076a4e7dfda350a12f3319be27eec349c652a905
suchasurge <frank@heidjer.info>
2014-03-21 17:20:59 +0100

    More CI testing

--- math_test.go
+++ math_test.go
@@ -2,8 +2,14 @@ package math
 
 import "testing"
 
-func TestAverage(t *testing.T) {
+func TestMath1(t *testing.T) {
     if 1 + 2 != 3 {
         t.Error("Expected 3, got ", 1 + 2)
+    }
+}
+
+func TestMath2(t *testing.T) {
+    if 10 + 20 != 30 {
+        t.Error("Expected 30, got ", 10 + 20)
     }
 }
